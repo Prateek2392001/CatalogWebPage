c2e23299a7e99246aa47a6149d30a1b4e138d20d
Prateek2392001 <103823442+Prateek2392001@users.noreply.github.com>
2025-07-04 14:51:20 +0530

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./Pages/About";
 import Services from "./Pages/Services";
@@ -8,45 +8,10 @@ import Contact from "./Pages/Contact";
 import Home from "./components/HomePage/Home";
 
 function App() {
-  useEffect(() => {
-    const scripts = [
-      "/assets/js/jquery.min.js",
-      "/assets/js/popper.js",
-      "/assets/js/bootstrap.min.js",
-      "/assets/js/plugin/slick.min.js",
-      "/assets/js/plugin/html5lightbox.js",
-      "/assets/js/placeholdem.min.js",
-      "/assets/js/wow.min.js",
-      "/assets/js/script.js",
-    ];
-
-    scripts.forEach((src) => {
-      const script = document.createElement("script");
-      script.src = src;
-      script.async = false;
-      document.body.appendChild(script);
-    });
-
-    return () => {
-      scripts.forEach((src) => {
-        const loadedScripts = document.querySelectorAll(`script[src="${src}"]`);
-        loadedScripts.forEach((script) => script.remove());
-      });
-    };
-  }, []);
-
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Home />
-            </>
-          }
-        />
-
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/portfolio" element={<Portfolio />} />
